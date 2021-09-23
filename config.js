86329ee9f533175bb269c6ba22111072d55a279d
trey43 <91261428+trey43@users.noreply.github.com>
2021-09-23 09:08:11 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+{
+exports.TOKEN = 'ODY2NDg3MjgyMDc2NzQ1NzQ4.YPTRKg.8tjLMoEgA1Yrhddb2jmpOiEBG6I';
+
+exports.PREFIX = '!';
+
+exports.news_API = '49f1a59be64b4aed8edaf273ecaef8aa';
+
+exports.giphy_API = 'IOzpM13qeKGbiMkTcF54FUOsDSE0lwNE';
+
+exports.AME_API = 'bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
+}
